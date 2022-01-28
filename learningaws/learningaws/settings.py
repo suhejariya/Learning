3e613b589666fd5d049fd46ea8e413319bbd5184
suhejariya <suhejariya@gmail.com>
2022-01-28 15:24:12 +0530

    Update settings.py
    Changes mysql to sqlite server

--- learningaws/learningaws/settings.py
+++ learningaws/learningaws/settings.py
@@ -74,27 +74,27 @@ WSGI_APPLICATION = 'learningaws.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'learn',
-        'USER': 'admin',
-        'PASSWORD': 'riya1234',
-        'HOST': 'learn-db.c2alxhjq6azg.us-east-2.rds.amazonaws.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'use_unicode': True
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'learn',
+#         'USER': 'admin',
+#         'PASSWORD': 'riya1234',
+#         'HOST': 'learn-db.c2alxhjq6azg.us-east-2.rds.amazonaws.com',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'charset': 'utf8mb4',
+#             'use_unicode': True
+#         },
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
